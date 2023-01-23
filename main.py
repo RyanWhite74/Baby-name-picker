7eb31ec0a9444fb011c1a7458af4ba21a8bdbbfd
Ryan White <>
2023-01-23 01:37:19 +0000

    Added main functionality

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+import random
+
+first_name = input("what is the babies first name? ")
+last_name = input ("What is the babies last name? ")
+
+real_name ="Rae"
+
+
+input_names = input("Enter middle names you like separated by a comma: ")
+
+lst = input_names.split(",")
+
+print("Your babies full name is " + first_name + " " + random.choice(lst) + " " + last_name)
