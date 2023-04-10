e92489f6f578b39c2de302fb808e77b72c583641
Ryan White <123340549+RyanWhite74@users.noreply.github.com>
2023-04-10 13:24:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 import random
 
-first_name = input("what is the babies first name? ")
-last_name = input ("What is the babies last name? ")
+first_name = input("what is the baby's first name? ")
+last_name = input ("What is the baby's last name? ")
 
 
 
@@ -11,4 +11,4 @@ input_names = input("Enter middle names you like separated by a comma: ")
 
 lst = input_names.split(",")
 
-print("Your babies full name is " + first_name + " " + random.choice(lst) + " " + last_name)
+print("Your baby's full name is " + first_name + " " + random.choice(lst) + " " + last_name)
